a80054332c219b751ca406abc2948bb3e3303382
yassineuuu <57989575+yassineuuu@users.noreply.github.com>
2020-04-27 02:03:54 +0000

    adjust precedent button

--- Starter-file/dist/js/script.js
+++ Starter-file/dist/js/script.js
@@ -42,77 +42,78 @@ test.addEventListener('click', function(){
         var width = 4;
         
         progress.width=width+"%";
+        function Question(){
+                
+
+            switch (i) {
+                case  1:
+                    document.getElementById('q/qs').innerHTML = `${data[i].id}/22`
+                    document.getElementById('para').innerHTML = `<h4>${data[i].qst}</h4>
+                                                                <div class="answer">
+                                                                <span><input type="number" name="Q2" id="température" min="34" max="42" placeholder="34 - 42">
+                                                                deg</span>
+                                                                </div>`
+                break;
+                case 9:
+                    document.getElementById('q/qs').innerHTML = `${data[i].id}/22`
+                    document.getElementById('para').innerHTML = `<h4>${data[i].qst}</h4>
+                                                                <div class="answer">
+                                                                <div><input type="radio" id="bien"><label for="bien"><i class="far fa-grin-alt"></i><h4>${data[i].answer[0]}</h4></label></div>
+                                                                <div><input type="radio" id="assez"><label for="assez"><i class="far fa-smile"></i><h4>${data[i].answer[1]}</h4></label></div>
+                                                                <div><input type="radio" id="fatigué"><label for="fatigué"><i class="far fa-grimace"></i><h4>${data[i].answer[2]}</h4></label></div>
+                                                                <div><input type="radio" id="trésfatigué"><label for="trésfatigué"><i class="far fa-tired"></i><h4>${data[i].answer[3]}</h4></label></div>
+                                                                </div>`
+                break;
+                case  10:
+                    document.getElementById('q/qs').innerHTML = `${data[i].id}/22`
+                    document.getElementById('para').innerHTML = `<h4>${data[i].qst}</h4>
+                                                                <div class="answer">
+                                                                <span><input type="number" name="Q2" id="age" min="15" max="100" placeholder="15 - 100">
+                                                                ans</span>
+                                                                </div>`
+                break;
+                case  11:
+                    document.getElementById('q/qs').innerHTML = `${data[i].id}/22`
+                    document.getElementById('para').innerHTML = `<h4>${data[i].qst}</h4>
+                                                                <div class="answer">
+                                                                <span><input type="number" name="Q2" id="poids" min="15" max="300" placeholder="Poids">
+                                                                kg</span>
+                                                                </div>`
+                break;
+                case  12:
+                    document.getElementById('q/qs').innerHTML = `${data[i].id}/22`
+                    document.getElementById('para').innerHTML = `<h4>${data[i].qst}</h4>
+                                                                <div class="answer">
+                                                                <span><input type="number" name="Q2" id="taille" min="140" max="300" placeholder="Taille">
+                                                                cm</span>
+                                                                </div>`
+                break;
+                case  19:
+                    document.getElementById('q/qs').innerHTML = `${data[i].id}/22`
+                    document.getElementById('para').innerHTML = `<h4>${data[i].qst}</h4>
+                                                                <div class="answer">
+                                                                <div><input type="radio" id="oui"><label for="oui"><h4>${data[i].answer[0]}</h4></label></div>
+                                                                <div><input type="radio" id="non"><label for="non"><h4>${data[i].answer[1]}</h4></label></div>
+                                                                <div><input type="radio" id="homme"><label for="homme"><i class="fas fa-male"></i><h4>${data[i].answer[2]}</h4></label></div>
+                                                                </div>`
+                break;
+                default:
+                    document.getElementById('q/qs').innerHTML = `${data[i].id}/22`
+                    document.getElementById('para').innerHTML = `<h4>${data[i].qst}</h4>
+                                                                <div class="answer">
+                                                                <div><input type="radio" id="oui"><label for="oui"><h4>${data[i].answer[0]}</h4></label></div>
+                                                                <div><input type="radio" id="non"><label for="non"><h4>${data[i].answer[1]}</h4></label></div>
+                                                                </div>`
+                break;
+            }
+        
+        }
         next.addEventListener('click', function(){
             i++;
             if(i>=22){
                 i=21
             }
-            function Question(){
-
-
-                switch (i) {
-                    case  1:
-                        document.getElementById('q/qs').innerHTML = `${data[i].id}/22`
-                        document.getElementById('para').innerHTML = `<h4>${data[i].qst}</h4>
-                                                                    <div class="answer">
-                                                                    <span><input type="number" name="Q2" id="température" min="34" max="42" placeholder="34 - 42">
-                                                                    deg</span>
-                                                                    </div>`
-                    break;
-                    case 9:
-                        document.getElementById('q/qs').innerHTML = `${data[i].id}/22`
-                        document.getElementById('para').innerHTML = `<h4>${data[i].qst}</h4>
-                                                                    <div class="answer">
-                                                                    <div><input type="radio" id="bien"><label for="bien"><i class="far fa-grin-alt"></i><h4>${data[i].answer[0]}</h4></label></div>
-                                                                    <div><input type="radio" id="assez"><label for="assez"><i class="far fa-smile"></i><h4>${data[i].answer[1]}</h4></label></div>
-                                                                    <div><input type="radio" id="fatigué"><label for="fatigué"><i class="far fa-grimace"></i><h4>${data[i].answer[2]}</h4></label></div>
-                                                                    <div><input type="radio" id="trésfatigué"><label for="trésfatigué"><i class="far fa-tired"></i><h4>${data[i].answer[3]}</h4></label></div>
-                                                                    </div>`
-                    break;
-                    case  10:
-                        document.getElementById('q/qs').innerHTML = `${data[i].id}/22`
-                        document.getElementById('para').innerHTML = `<h4>${data[i].qst}</h4>
-                                                                    <div class="answer">
-                                                                    <span><input type="number" name="Q2" id="age" min="15" max="100" placeholder="15 - 100">
-                                                                    ans</span>
-                                                                    </div>`
-                    break;
-                    case  11:
-                        document.getElementById('q/qs').innerHTML = `${data[i].id}/22`
-                        document.getElementById('para').innerHTML = `<h4>${data[i].qst}</h4>
-                                                                    <div class="answer">
-                                                                    <span><input type="number" name="Q2" id="poids" min="15" max="300" placeholder="Poids">
-                                                                    kg</span>
-                                                                    </div>`
-                    break;
-                    case  12:
-                        document.getElementById('q/qs').innerHTML = `${data[i].id}/22`
-                        document.getElementById('para').innerHTML = `<h4>${data[i].qst}</h4>
-                                                                    <div class="answer">
-                                                                    <span><input type="number" name="Q2" id="taille" min="140" max="300" placeholder="Taille">
-                                                                    cm</span>
-                                                                    </div>`
-                    break;
-                    case  19:
-                        document.getElementById('q/qs').innerHTML = `${data[i].id}/22`
-                        document.getElementById('para').innerHTML = `<h4>${data[i].qst}</h4>
-                                                                    <div class="answer">
-                                                                    <div><input type="radio" id="oui"><label for="oui"><h4>${data[i].answer[0]}</h4></label></div>
-                                                                    <div><input type="radio" id="non"><label for="non"><h4>${data[i].answer[1]}</h4></label></div>
-                                                                    <div><input type="radio" id="homme"><label for="homme"><i class="fas fa-male"></i><h4>${data[i].answer[2]}</h4></label></div>
-                                                                    </div>`
-                    break;
-                    default:
-                        document.getElementById('q/qs').innerHTML = `${data[i].id}/22`
-                        document.getElementById('para').innerHTML = `<h4>${data[i].qst}</h4>
-                                                                    <div class="answer">
-                                                                    <div><input type="radio" id="oui"><label for="oui"><h4>${data[i].answer[0]}</h4></label></div>
-                                                                    <div><input type="radio" id="non"><label for="non"><h4>${data[i].answer[1]}</h4></label></div>
-                                                                    </div>`
-                    break;
-                }
             
-            }
             Question();
             
             
@@ -141,12 +142,13 @@ test.addEventListener('click', function(){
             if(i<=0){
                 i=0
             }
-            document.getElementById('q/qs').innerHTML = `${data[i].id}/22`
-            document.getElementById('para').innerHTML = `<h4>${data[i].qst}</h4>
-                                                    <div class="answer">
-                                                    <div><input type="radio" id="oui"><label for="oui"><h4>${data[i].answer[0]}</h4></label></div>
-                                                    <div><input type="radio" id="non"><label for="non"><h4>${data[i].answer[1]}</h4></label></div>
-                                                    </div>`
+            // document.getElementById('q/qs').innerHTML = `${data[i].id}/22`
+            // document.getElementById('para').innerHTML = `<h4>${data[i].qst}</h4>
+            //                                         <div class="answer">
+            //                                         <div><input type="radio" id="oui"><label for="oui"><h4>${data[i].answer[0]}</h4></label></div>
+            //                                         <div><input type="radio" id="non"><label for="non"><h4>${data[i].answer[1]}</h4></label></div>
+            //                                         </div>`
+            Question();
             
             function progressBarBack(){
                 
